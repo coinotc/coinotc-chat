1fc0269fac79df09d783414e1f8c98bf74032f38
Kenneth Phang <bunnyppl@gmail.com>
2018-01-03 20:31:46 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    let config = {
+      apiKey: "AIzaSyDnDHVLJ2H3mI-eXBF9oldYhr9olRkalKs",
+      authDomain: "chat-cb8c1.firebaseapp.com",
+      databaseURL: "https://chat-cb8c1.firebaseio.com",
+      projectId: "chat-cb8c1",
+      storageBucket: "chat-cb8c1.appspot.com",
+      messagingSenderId: "265407788958"
+    };
+    firebase.initializeApp(config);
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
+
---
+++ src/pages/home/home.ts
@@ -0,0 +1,103 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController,AlertController, Events } from 'ionic-angular';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit{
+	client: any;
+	ref;
+	typingStatus;
+	name;
+	newmessage;
+	messagesList;
+	typing = '';
+	typeStatusId;
+  
+  constructor(public navCtrl: NavController, public alert: AlertController, public events: Events) {
+		this.ref = firebase.database().ref('messages');
+		this.typingStatus = firebase.database().ref('typeStatus');
+		this.name = '';
+	}
+	
+	ngOnInit() { 
+		// Handle is typing event
+		this.typingStatus.on('value',data => {
+  		data.forEach( data => {
+				if(typeof(data.val().name) !== 'undefined'){
+					if(data.val().name !== this.name.username){
+						this.typing = data.val().name + ' is typing...'
+						setTimeout(() => {
+							this.typing = ''
+						}, 2000)
+					}
+				}
+  		});
+  	});
+		
+	}
+ 
+	 onChange(e) {
+		this.newmessage = e.target.value;
+		//this.events.publish('chat:typing', this.name);
+		this.typingStatus.push({
+			name: this.name.username,
+			datetime: Date.now()
+		});
+ 	}
+
+  ionViewDidLoad(){
+  	// Presenting popup
+  	this.alert.create({
+  		title:'Username',
+  		inputs:[{
+  			name:'username',
+  			placeholder: 'username'
+  		}],
+  		buttons:[{
+  			text: 'Continue',
+  			handler: username =>{
+					this.name = username;
+					// need to check list of logout users remove them as well. 
+					this.typingStatus.on('value',data => {
+						data.forEach( data => {
+							console.log(data.val().id !== this.typeStatusId);
+							if(typeof(data.val().name) !== 'undefined'){
+								if(data.val().name === username){
+									data.remove();
+								}
+							}
+						});
+					});
+  			}
+  		}]
+		}).present();
+		
+		//reading data from firebase
+  	this.ref.on('value',data => {
+  		let tmp = [];
+  		data.forEach( data => {
+  			tmp.push({
+  				key: data.key,
+  				name: data.val().name,
+  				message: data.val().message
+  			})
+  		});
+  		this.messagesList = tmp;
+		});
+		
+  }
+  
+  // send message
+  send(){
+  	// add new data to firebase
+  	this.ref.push({
+  		name: this.name.username,
+  		message: this.newmessage
+		});
+		this.newmessage = '';
+  }
+
+}
